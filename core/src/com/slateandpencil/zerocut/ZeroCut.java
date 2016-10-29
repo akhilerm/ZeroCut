1e5b6a13b240f6ce60bd003875f0b1adb4c64864
Akhil Mohan <akhilerm@gmail.com>
2016-10-29 20:23:22 +0530

    Logo added Logo added successfully for all platforms

--- core/src/com/slateandpencil/zerocut/ZeroCut.java
+++ core/src/com/slateandpencil/zerocut/ZeroCut.java
@@ -8,12 +8,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ZeroCut extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	//Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//img = new Texture("badlogic.jpg");
 	}
 
 	@Override
@@ -21,13 +21,13 @@ public class ZeroCut extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		//img.dispose();
 	}
 }
--- desktop/src/com/slateandpencil/zerocut/desktop/DesktopLauncher.java
+++ desktop/src/com/slateandpencil/zerocut/desktop/DesktopLauncher.java
@@ -4,9 +4,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.slateandpencil.zerocut.ZeroCut;
 
+import java.nio.file.Files;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.addIcon("ic_launcher_128.png", com.badlogic.gdx.Files.FileType.Internal);
+        config.addIcon("ic_launcher_32.png", com.badlogic.gdx.Files.FileType.Internal);
+        config.addIcon("ic_launcher_16.png", com.badlogic.gdx.Files.FileType.Internal);
+
 		new LwjglApplication(new ZeroCut(), config);
 	}
 }
