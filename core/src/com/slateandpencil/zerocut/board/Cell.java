98502cd60862ce9cd0edd62252463ec4e6b3c87e
Akhil Mohan <akhilerm@gmail.com>
2016-11-06 10:41:28 +0530

    Cell class created Cell class contains both the cell value and cellposition of any cell. It also has functions to manipulate all the attributes.

--- core/src/com/slateandpencil/zerocut/board/Cell.java
+++ core/src/com/slateandpencil/zerocut/board/Cell.java
@@ -9,4 +9,30 @@ public class Cell {
     public enum CellValue {
         EMPTY, NOUGHT, CROSS
     }
+
+    CellPosition position;
+    CellValue value;
+
+    public Cell(Cell cell) {
+        this.position = new CellPosition(cell.position);
+        this.value = cell.value;
+    }
+
+    public Cell(CellPosition position) {
+        this.position = position;
+        this.value = CellValue.EMPTY;
+    }
+
+    public Cell(CellPosition position, CellValue value) {
+        this.position = position;
+        this.value = value;
+    }
+
+    public CellValue getValue() {
+        return value;
+    }
+
+    public CellPosition getPosition() {
+        return position;
+    }
 }
